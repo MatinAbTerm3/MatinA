e0a5a8c822bb5f8822f1528f6f4927b912c14537
MatinAbTerm3 <79203163+MatinAbTerm3@users.noreply.github.com>
2021-04-14 17:45:55 +0430

    Add files via upload

---
+++ includes/view/templates/header.php
@@ -0,0 +1,40 @@
+<header>
+			<nav class="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.html"><img src="assets/images/icon.png" alt="لوگو" width="45" height="48" class="d-inline-block align-top"></a>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    ورود/ثبت نام
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li><a class="dropdown-item" href="register.html">ثبت نام</a></li>
+                                    <li><a class="dropdown-item" href="#">ورود</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.html">خانه</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    کتاب ها
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                                    <li><a class="dropdown-item" href="#">کتاب های نو</a></li>
+                                    <li><a class="dropdown-item" href="#">کتاب های دست دوم</a></li>
+                                    <li><a class="dropdown-item" href="#">کتاب های صوتی</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="contactus.html" >تماس با ما</a>
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="مثال: تاریخچه زمان" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">بگرد</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </header>
